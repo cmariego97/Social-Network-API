119d0df9a5bc0310671dae004af2d2af5854182e
cmariego97 <cmarie.go97@gmail.com>
2022-10-30 15:15:49 -0400

    added routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  removeUser,
+  addFriend,
+  removeFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/')
+  .get(getUsers)
+  .post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(removeUser)
+
+//api/users/:userId/:friendId
+router.route('/:userId/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
+
+module.exports = router;
+Footer
